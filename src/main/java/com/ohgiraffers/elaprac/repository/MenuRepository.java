f45f8d1c8c4e0b3207c3d889c53a89f4c3e6af81
CJC0512 <ehdchs2006@gmail.com>
2024-05-08 10:28:48 +0900

    first commit

---
+++ src/main/java/com/ohgiraffers/elaprac/repository/MenuRepository.java
@@ -0,0 +1,8 @@
+package com.ohgiraffers.elaprac.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ohgiraffers.elaprac.entitiy.Menu;
+
+public interface MenuRepository extends JpaRepository<Menu,Integer> {
+}
